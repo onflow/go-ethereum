aea76cf001a07397a132f4b04dd47cce535fe03c
Ardit Marku <markoupetr@gmail.com>
2025-07-28 13:56:37 +0300

    Update import for c-kzg-4844 package

--- crypto/kzg4844/kzg4844_ckzg_cgo.go
+++ crypto/kzg4844/kzg4844_ckzg_cgo.go
@@ -24,7 +24,7 @@ import (
 	"sync"
 
 	gokzg4844 "github.com/crate-crypto/go-eth-kzg"
-	ckzg4844 "github.com/onflow/c-kzg-4844/v2/bindings/go"
+	ckzg4844 "github.com/ethereum/c-kzg-4844/v2/bindings/go"
 	"github.com/onflow/go-ethereum/common/hexutil"
 )
 
